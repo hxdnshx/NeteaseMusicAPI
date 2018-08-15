2bcc4b767ab70c45f4a4d3d48704ac856e8ce080
Infko <recollectionforgot@gmail.com>
2018-08-15 08:50:56 +0800

    try add multi-framework support

---
+++ publish.ps1
@@ -0,0 +1,2 @@
+dotnet pack
+dotnet nuget push .\NeteaseMusicAPI\bin\Debug\NeteaseMusicAPI.1.0.0.nupkg -k oy2ctjdwwqktdtq6xjdq2i45jdxceigelad7luyb3ry54m -s https://api.nuget.org/v3/index.json
--- src/AssemblyInfo.cs
+++ src/AssemblyInfo.cs
@@ -9,6 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("1.0.1")]
 
 //将 ComVisible 设置为 false 将使此程序集中的类型
 //对 COM 组件不可见。  如果需要从 COM 访问此程序集中的类型，
